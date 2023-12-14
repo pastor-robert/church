eb97db1295730fee5308f09a396f2f7ee325f4fe
Rob Adams <rob@rob-adams.us>
2023-12-14 01:56:56 -0600

    Refactor views and templates

--- church/views.py
+++ church/views.py
@@ -21,37 +21,7 @@ def about(request):
     return render(request, "church/about.html", {})
 
 def home(request):
-    """
-    This view returns the home page of the church. It also handles the POST request
-    for the PersonForm. If the form is valid, it saves the form and sets success to True.
-    
-    :param request: The HTTP request object.
-    :return: The rendered home page.
-    """
-    addresses = Address.objects.all()
-    success = False
-    google_api_key_set = bool(settings.GOOGLE_API_KEY)
-    person_set = Person.objects.all()
-
-    if request.method == "POST":
-        form = PersonForm(request.POST, request.FILES)
-        if form.is_valid():
-            success = True
-            form.save()
-            messages.success(request, f"Succesfully added {form.cleaned_data['name']} to the database.")
-        else: 
-            messages.error(request, "Please correct the errors below.") 
-
-    form = PersonForm()
-
-    context = {
-            "form": form,
-            "google_api_key_set": google_api_key_set,
-            "success": success,
-            "addresses": addresses,
-            "person_set": person_set,
-            }
-    return render(request, "church/home.html", context)
+    return render(request, "church/home.html", {})
 
 def church(request):
     """
--- directory/views.py
+++ directory/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib import messages
+from address.models import Address
+
+from church import settings
+from person.models import Person
+from person.forms import PersonForm
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Directory Home</h1>")
+    """
+    This view returns the church directory.
+
+    :param request: The HTTP request object.
+    :return: The rendered home page.
+    """
+    addresses = Address.objects.all()
+    success = False
+    google_api_key_set = bool(settings.GOOGLE_API_KEY)
+    person_set = Person.objects.all()
+
+    if request.method == "POST":
+        form = PersonForm(request.POST, request.FILES)
+        if form.is_valid():
+            success = True
+            form.save()
+            messages.success(request, f"Succesfully added {form.cleaned_data['name']} to the database.")
+        else: 
+            messages.error(request, "Please correct the errors below.") 
+
+    form = PersonForm()
+
+    context = {
+            "form": form,
+            "google_api_key_set": google_api_key_set,
+            "success": success,
+            "addresses": addresses,
+            "person_set": person_set,
+            }
+    return render(request, "directory/home.html", context)
